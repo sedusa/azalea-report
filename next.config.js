b3e3446c95d5a3770a9cf83c42f4cef5f643d628
sedusa <samuel.edusa@gmail.com>
2024-09-11 21:15:59 -0400

    => Updated config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,16 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
   webpack: (cfg) => {
-      cfg.module.rules.push(
-          {
-              test: /\.md$/,
-              loader: 'frontmatter-markdown-loader',
-              options: { mode: ['react-component'] }
-          }
-      )
-      return cfg
+    cfg.module.rules.push(
+      {
+        test: /\.md$/,
+        loader: 'frontmatter-markdown-loader',
+        options: { mode: ['react-component'] }
+      }
+    )
+    return cfg
   }
 }
+
+module.exports = nextConfig
