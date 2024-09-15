7e1fdb4858383f5682a52245fb472076b0a51b32
sedusa <samuel.edusa@gmail.com>
2024-09-15 19:02:05 -0400

    => Updated next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  images: {
+    unoptimized: true,
+  },
   webpack: (cfg) => {
     cfg.module.rules.push(
       {
